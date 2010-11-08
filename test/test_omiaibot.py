672cabe62f522b8781622911c10570074fd3f33e
sinsoku <sinsoku.listy@gmail.com>
2010-11-08 20:12:24 +0900

    モッククラスをmock.pyに移動

--- test/test_omiaibot.py
+++ test/test_omiaibot.py
@@ -2,24 +2,10 @@
 # -*- coding:utf-8 -*-
 
 from nose.tools import *
+from mock import MockStatus
 from models import UserModel, StatusModel
 from omiaibot import OmiaiBot
 from StringIO import StringIO
-
-
-class MockUser(object):
-
-    def __init__(self, id, screen_name):
-        self.id = id
-        self.screen_name = screen_name
-
-
-class MockStatus(object):
-
-    def __init__(self, id, user_id, screen_name, text):
-        self.id = id
-        self.author = MockUser(user_id, screen_name)
-        self.text = text
 
 
 class TestOmiaiBot(object):
